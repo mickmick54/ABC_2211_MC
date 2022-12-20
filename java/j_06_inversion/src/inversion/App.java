60485662863775677fc46e1ec148d364e2bb9b69
Mickaël Colleu <6249403+mickmick54@users.noreply.github.com>
2022-12-20 16:12:39 +0100

    sauvegarde avant reinitialisation 2

---
+++ java/j_06_inversion/src/inversion/App.java
@@ -0,0 +1,59 @@
+package inversion;
+
+import java.util.Scanner;
+
+public class App {
+
+	/*
+	 
+	1)	Lire a
+	2)	Ecrire « Donnez la valeur de A » A
+	3)	Lire b
+	4)	Ecrire « Donnez la valeur de B » B
+	5)	Temp  a
+	6)	a  b
+	7)	b  temp
+	8)	Ecrire « A »
+	9)	Ecrire « B »
+
+	 
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 0;
+		
+		int b = 0;
+		
+		int tmp = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Lire 2 nombres entiers et les inverser");
+		
+		System.out.println("Saisir le nombre a");
+		
+		a = sc.nextInt();
+		
+		System.out.println("Vous avez saisi : " + a);
+		
+		System.out.println("Saisir le nombre b");
+		
+		b = sc.nextInt();
+		
+		System.out.println("Vous avez saisi : " + b);
+		
+		tmp = a;
+		
+		a = b;
+		
+		b = tmp;
+		
+		System.out.println("Affichage de " + a);
+		
+		System.out.println("Affichage de " + b);
+		
+	}
+
+}
