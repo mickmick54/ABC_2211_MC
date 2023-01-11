8fdf32c244afaba82cd79d8aa457017a7e2daae8
Mickaël Colleu <6249403+mickmick54@users.noreply.github.com>
2023-01-11 08:46:40 +0100

    update lettre.app

--- java/j_32_fourchette_correction/src/fourchette_correction/App.java
+++ java/j_32_fourchette_correction/src/fourchette_correction/App.java
@@ -1,6 +1,43 @@
 package fourchette_correction;
 
 public class App {
+	
+	/*
+	 VARIABLES
+	 	min est un entier
+	 	max est un entier
+	 	nb est un entier générer aléatoirement
+	 	nbJoueur est un entier saisie par le joueur
+	 	borneMin est un entier
+	 	borneMax est un entier
+	 	nbEssais est un entier
+	 
+	 DEBUT DU PROGRAMME
+	 
+	 	Tant que nb est différent de nbJoueur 
+	 		Faire
+	 			Ecrire "Choisissez un nombre entre ",borneMin," et ",borneMax
+	 			Lire nbJoueur
+	 			Si nb est egale à nbJoueur 
+	 				Alors
+	 					Ecrire "Vous avez gagné en " ,nbEssais, " essais"
+	 			Fin si
+	 			Si nb est inférieur à nbJoueur
+	 				Alors
+	 					Ecrire "C'est moins"
+	 					borneMax = nbJoueur - 1
+	 			Fin Si
+	 			Si nb est supèrieur à nbJoueur
+	 				Alors
+	 					Ecrire "C'est plus"
+	 					borneMin = nbJoueur + 1
+	 			Fin Si
+	 			nbEssais + 1
+	 	Fin Tant que
+	 
+	 FIN DU PROGRAMME
+	 
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ java/j_33_lettre/src/lettre/App.java
@@ -0,0 +1,38 @@
+package lettre;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int count = 0;
+		
+		char target = 'a';
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Entrez une chaîne de caractères terminée par le caractère '.' : ");
+		
+		String input = scanner.nextLine();
+		
+		if (input.length() == 0 || (input.length() == 1 && input.charAt(0) == '.')) {
+			
+		    System.out.println("LA CHAINE EST VIDE");
+		    
+		} else {
+			
+		    for (int i = 0; i < input.length(); i++) {
+		    	
+		        if (input.charAt(i) == target) {
+		        	
+		            count++;
+		        }
+		    }
+		    
+		    System.out.println("Le nombre d'occurrences de la lettre '" + target + "' est : " + count);
+		}
+	}
+
+}
