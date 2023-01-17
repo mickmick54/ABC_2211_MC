10bd2b7035c615b221e10af170effb242c249aaa
Mickaël Colleu <6249403+mickmick54@users.noreply.github.com>
2023-01-17 10:48:28 +0100

    Update fourchette

--- java/j_32_fourchette_correction/src/fourchette_correction/App.java
+++ java/j_32_fourchette_correction/src/fourchette_correction/App.java
@@ -1,5 +1,8 @@
 package fourchette_correction;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
 	
 	/*
@@ -41,7 +44,37 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int nb;
+		int nbJoueur = 0;
+		int borneMin = 0;
+		int borneMax = 100;
+		int nbEssais = 0;
 		
+		Random aleatoire = new Random();
+	      
+	    nb = aleatoire.nextInt(101);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (nb != nbJoueur) {
+			
+			System.out.println("Choisissez un nombre entre " + borneMin + " et " + borneMax);
+			
+			nbJoueur = sc.nextInt();
+			
+			if (nb == nbJoueur) {
+				System.out.println("Vous avez gagné en " + nbEssais + " essais");
+			}
+			else if (nb < nbJoueur) {
+				System.out.println("C'est moins");
+				borneMax = nbJoueur - 1;
+			}
+			else {
+				System.out.println("C'est plus");
+				borneMin = nbJoueur + 1;
+			}
+			nbEssais++;
+		}
+		sc.close();
 	}
-
 }
