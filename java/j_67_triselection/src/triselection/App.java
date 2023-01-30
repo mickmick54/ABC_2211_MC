6a5fac0bd90a134a2c9cd6d9421dfa2f8318dca1
Mickaël Colleu <6249403+mickmick54@users.noreply.github.com>
2023-01-30 09:58:49 +0100

    save tri

--- java/j_67_triselection/src/triselection/App.java
+++ java/j_67_triselection/src/triselection/App.java
@@ -1,6 +1,44 @@
 package triselection;
 
+import java.util.Random;
+
 public class App {
+	
+	public static void main(String[] args) {
+		  
+	    int n = 10;
+	    
+	    int[] t = new int[n];
+	    
+	    Random random = new Random();
+
+	    for (int i = 0; i < n; i++) {
+	    	
+	      t[i] = random.nextInt(100);
+	    }
+
+	    System.out.println("Tableau non trié :");
+	    
+	    for (int i = 0; i < n; i++) {
+	    	
+	      System.out.print(t[i] + " ");
+	      
+	    }
+	    
+	    System.out.println();
+
+	    selectionSort(t);
+
+	    System.out.println("Tableau trié :");
+	    
+	    for (int i = 0; i < n; i++) {
+	    	
+	      System.out.print(t[i] + " ");
+	      
+	    }
+	    
+	    System.out.println();
+	  }
 
 	public static void selectionSort(int[] t) {
 		
--- java/j_68_tribulle/src/tribulle/App.java
+++ java/j_68_tribulle/src/tribulle/App.java
@@ -1,27 +1,62 @@
 package tribulle;
 
+import java.util.Random;
+
 public class App {
+	
+  public static void main(String[] args) {
+	  
+    int n = 10;
+    
+    int[] t = new int[n];
+    
+    Random random = new Random();
 
-	public static void bubbleSort(int[] t) {
-		
-		  int n = t.length;
-		  
-		  for (int i = 0; i < n - 1; i++) {
-			  
-		    for (int j = 0; j < n - i - 1; j++) {
-		    	
-		      if (t[j] > t[j + 1]) {
-		    	  
-		        int temp = t[j];
-		        
-		        t[j] = t[j + 1];
-		        
-		        t[j + 1] = temp;
-		        
-		      }
-		    }
-		  }
-		}
+    for (int i = 0; i < n; i++) {
+    	
+      t[i] = random.nextInt(100);
+    }
 
+    System.out.println("Tableau non trié :");
+    
+    for (int i = 0; i < n; i++) {
+    	
+      System.out.print(t[i] + " ");
+      
+    }
+    
+    System.out.println();
 
+    bubbleSort(t);
+
+    System.out.println("Tableau trié :");
+    
+    for (int i = 0; i < n; i++) {
+    	
+      System.out.print(t[i] + " ");
+      
+    }
+    
+    System.out.println();
+  }
+
+  public static void bubbleSort(int[] t) {
+	  
+    int n = t.length;
+    
+    for (int i = 0; i < n - 1; i++) {
+    	
+      for (int j = 0; j < n - i - 1; j++) {
+    	  
+        if (t[j] > t[j + 1]) {
+        	
+          int temp = t[j];
+          
+          t[j] = t[j + 1];
+          
+          t[j + 1] = temp;
+        }
+      }
+    }
+  }
 }
